b6582ba904a9df42adf3841ee47210dd690b573f
nobreconfrade <willpereiraBR@gmail.com>
2016-05-09 11:44:40 -0300

    ver 1.02

--- nt_csv.py
+++ nt_csv.py
@@ -5,15 +5,16 @@ Author: William Pereira
 
 An RDF NT -> CSV translator, hope it works.
 
-Version 1.01
+Version 1.02
 
 -Saving 'line' in 'newline' on CSV format; CHECKED
 -Save 'newline' on the 'csv_data.csv' file; CHECKED
 -Saving 'line' in 'newline' without a (' ') in the endline; CHECKED
--Saving 'line' in 'newline' without ('"') in literal cases;
+-Saving 'line' in 'newline' without ('"') in literal cases; CHECKED
+-Saving 'line' in 'newline' without ('@**') in comment object cases (I'm eager to know why this exists in a first place); CHECKED
 
 Neo4j sample command to import: 	USING PERIODIC COMMIT 10000
-									LOAD CSV FROM 'file:///csvdata_100.csv' AS line 
+									LOAD CSV FROM 'file:///csv_data100.csv' AS line 
 									CREATE (:BSBM{subject: line[0],predicate: line[1],object: line[2]})
 
 """
@@ -31,16 +32,18 @@ with open(nt_file,'r', buffering = 4096) as f1:
 			for enum_letter,letter in enumerate(line):
 				# print (letter)	
 				# print (enum_letter,' ',letter)
-				if(letter == '<'):
+				if(letter == '<' and line[enum_letter - 1] != '^'):
 					newline += letter.replace('<','"')
+				elif(letter == '<' and line[enum_letter - 1] == '^'):
+					newline += letter.replace('<','')
 				elif(letter == '>'):
 					newline += letter.replace('>','"')
 				elif(letter == ' ' and space_count<2):
 					newline += letter.replace(' ',',')
 					space_count += 1
-				elif((letter == '"' and line[enum_letter + 1] == '^') or (letter == '<' and line[enum_letter - 1] == '^')): # CHECK THIS SHIIII...
+				elif(letter == '"' and line[enum_letter + 1] == '^'): 
 					newline += letter.replace('"','')	
-				elif(letter == ' ' and line[enum_letter + 1] == '.'):
+				elif((letter == ' ' and line[enum_letter + 1] == '.') or (letter == '@' and newline[enum_letter - 1] == '"')):
 					break
 				else:
 					newline += letter
