fb39934839e9f77d8ac098e63d500afd57f4851c
nobreconfrade <willpereiraBR@gmail.com>
2016-06-15 16:42:26 -0300

    productType coded, need test

--- outerheaven.py
+++ outerheaven.py
@@ -5,6 +5,9 @@ Author: William Pereira
 
 For Python ver: 2.7
 
+////////'mold' cheat sheet:
+
+1 - ProductType
 """
 from neo4j.v1 import GraphDatabase, basic_auth
 
@@ -15,34 +18,45 @@ def connectDB():
 
 session = connectDB()
 
-producer = "producer1"
-label = "desensitizations bleachers gentries"	
-datex = "2003-06-15"
-comment = "journalism tramroads chemoreceptive pipets wordless accumulated finches grazed packers eggbeater feebler epileptoid monadism sickened vexes mensas marimbas christianizing leary snowbound"
+subclassof = ""
 
-session.run("CREATE (%s:Producer {label:{label},datex:{datex},comment:{comment}})"%producer,{"label":label,"datex":datex,"comment":comment})
+with open('data_100.ttl','r', buffering = 4096) as f1:
+	print ("--------------File successfully loaded.--------------")
+	for line in f1:
+		if('    ' in line):
+#######################################################################################################			
+			if(mold = 1): 
+				if('rdf:type' in line):
+					pass
+				elif('rdfs:label' in line):
+					label = line.replace("rdfs:label ","")
+					label = line.replace(" ;","")
+				elif('rdfs:subClassOf' in line):
+					subclassof = line.replace("rdfs:subClassOf bsbm-inst:","")
+					subclassof = line.replace(" ;","")
+				elif('rdfs:comment' in line):
+					comment = line.replace("rdfs:comment ","")
+					comment = line.replace(" ;","")
+				elif('dc:publisher' in line):
+					publisher = line.replace("dc:publisher <","")
+					publisher = line.replace("> ;","")
+				elif('dc:date' in line):
+					date = line.replace("dc:date ","")
+					date = line.replace("^^xsd:date .","")
+					# QUERY
+					session.run("CREATE ("+header+":ProductType {header:\""+header+"\",label:"+label+",comment:"+comment+",date:"+date+"})")
+					if(subclassof == ""):
+						session.run("CREATE (publisher:Publisher {header:\""+publisher+"\"})")
+					else:
+						session.run("MATCH (son{header:\""+header+"\"}),(father{header:\""+subclassof+"\"}) CREATE son-[:subClassOf]->father")
+					session.run("MATCH (son{header:\""+header+"\"}),(father{header:\""+publisher+"\"}) CREATE son-[:publisher]->father")
+				else:
+					print("Well, this is embarrassing... mold="+mold)
+#######################################################################################################			
+										
+		else:
+			if('bsbm-inst:ProductType' in line):
+				header = line.replace("bsbm-inst:","")
+				mold = 1
 
-product = "product1"
-label = "turgescence retrospection"	
-datex = "2000-11-01"
-comment = "attaining requirement legitimately overfatigue blancmange pedestrians druidisms commonwealths woofs hippie muddled livest upstages entourages denominations agrees salves perched theatergoers rigorists nae archimandrite cherty bingos hobnobbed praxeological parsable tetralogy imputers altruisms erecters assegais insinuator overwhelming homemaker conjunctiva filespec adaptability ionizes monogrammed moisteners unburdening walkings invocations riffled salaaming pedantically footworn semplice potholders flasks loopholes actually gushes coworkers cheapening beadroll immensely sibilance ombres stifling extremists"
-propertyNumeric1 = "831"
-propertyNumeric2 = "312"
-propertyNumeric3 = "735"
-propertyText1 = "nontenure declined grubs graybacks barmiest reflective flinching staffs spieler pittance underlips gainsays subcompact"
-propertyText2 = "anymore alternation alchemies unplugs almshouses obdurately cattish triskaidekaphobe heroics"
-propertyText3 = "tempers addresses sprinklers disabuses posterities phosgenes evacuation gaudies downer appeared quadrics disguisements fatalism"
-
-session.run("CREATE (%s:Product {label:{label},datex:{datex},comment:{comment},propertyNumeric1:{propertyNumeric1},"
-"propertyNumeric2:{propertyNumeric2},propertyNumeric3:{propertyNumeric3},propertyText1:{propertyText1},"
-"propertyText2:{propertyText2},propertyText3:{propertyText3}})"%product, {"label":label,"datex": datex, "comment":comment,
-"propertyNumeric1":propertyNumeric1, "propertyNumeric2":propertyNumeric2, "propertyNumeric3":propertyNumeric3,
-"propertyText1":propertyText1, "propertyText2":propertyText2, "propertyText3":propertyText3}) 
-
-relation = "bsbm:producer"
-
-session.run("CREATE ("+producer+") -[:"+relation+"]-> ("+product+")")
-# result = session.run("MATCH (a:Person) WHERE a.name = 'Arthur' RETURN a.name AS name, a.title AS title")
-# for record in result:
-# 	print("%s %s" % (record["title"], record["name"]))
 session.close()
---
+++ outerheaventest.py
@@ -0,0 +1,48 @@
+"""
+PRAISE THE SUN
+
+Author: William Pereira
+
+For Python ver: 2.7
+
+"""
+from neo4j.v1 import GraphDatabase, basic_auth
+
+def connectDB():
+	driver = GraphDatabase.driver("bolt://localhost", auth=basic_auth("neo4j", "admin"))
+	session = driver.session()	
+	return session
+
+session = connectDB()
+
+producer = "producer1"
+label = "desensitizations bleachers gentries"	
+datex = "2003-06-15"
+comment = "journalism tramroads chemoreceptive pipets wordless accumulated finches grazed packers eggbeater feebler epileptoid monadism sickened vexes mensas marimbas christianizing leary snowbound"
+
+session.run("CREATE (%s:Producer {label:{label},datex:{datex},comment:{comment}})"%producer,{"label":label,"datex":datex,"comment":comment})
+
+product = "product1"
+label = "turgescence retrospection"	
+datex = "2000-11-01"
+comment = "attaining requirement legitimately overfatigue blancmange pedestrians druidisms commonwealths woofs hippie muddled livest upstages entourages denominations agrees salves perched theatergoers rigorists nae archimandrite cherty bingos hobnobbed praxeological parsable tetralogy imputers altruisms erecters assegais insinuator overwhelming homemaker conjunctiva filespec adaptability ionizes monogrammed moisteners unburdening walkings invocations riffled salaaming pedantically footworn semplice potholders flasks loopholes actually gushes coworkers cheapening beadroll immensely sibilance ombres stifling extremists"
+propertyNumeric1 = "831"
+propertyNumeric2 = "312"
+propertyNumeric3 = "735"
+propertyText1 = "nontenure declined grubs graybacks barmiest reflective flinching staffs spieler pittance underlips gainsays subcompact"
+propertyText2 = "anymore alternation alchemies unplugs almshouses obdurately cattish triskaidekaphobe heroics"
+propertyText3 = "tempers addresses sprinklers disabuses posterities phosgenes evacuation gaudies downer appeared quadrics disguisements fatalism"
+
+session.run("CREATE (%s:Product {label:{label},datex:{datex},comment:{comment},propertyNumeric1:{propertyNumeric1},"
+"propertyNumeric2:{propertyNumeric2},propertyNumeric3:{propertyNumeric3},propertyText1:{propertyText1},"
+"propertyText2:{propertyText2},propertyText3:{propertyText3}})"%product, {"label":label,"datex": datex, "comment":comment,
+"propertyNumeric1":propertyNumeric1, "propertyNumeric2":propertyNumeric2, "propertyNumeric3":propertyNumeric3,
+"propertyText1":propertyText1, "propertyText2":propertyText2, "propertyText3":propertyText3}) 
+
+relation = "producer"
+
+session.run("CREATE ("+producer+") -[:"+relation+"]-> ("+product+")")
+# result = session.run("MATCH (a:Person) WHERE a.name = 'Arthur' RETURN a.name AS name, a.title AS title")
+# for record in result:
+# 	print("%s %s" % (record["title"], record["name"]))
+session.close()
