c316f475c57a562e25b6a2a0efa0539a3f8ed235
nobreconfrade <willpereiraBR@gmail.com>
2016-06-17 16:43:37 -0300

    need to revise Producer class

--- outerheaven.py
+++ outerheaven.py
@@ -10,8 +10,19 @@ For Python ver: 2.7
 0  - exit
 1  - ProductType
 2  - ProductFeature
+3  - Producer
+4  - Product
 """
 from neo4j.v1 import GraphDatabase, basic_auth
+
+
+def checkCountry(country):
+	auxCountry = country.split()
+	if(auxCountry in listCountry): #somthing wrong here
+		return True
+	else:
+		listCountry = listCountry + auxCountry
+		return False
 
 def connectDB():
 	driver = GraphDatabase.driver("bolt://localhost", auth=basic_auth("neo4j", "admin"))
@@ -20,9 +31,12 @@ def connectDB():
 
 session = connectDB()
 
+# PERTINENT VARIABLES
 subclassof = ""
+listCountry = []
 
-with open('data_100.ttl','r', buffering = 4096) as f1:
+# PROGRAM
+with open('data_1000.ttl','r', buffering = 4096) as f1:
 	print ("--------------File successfully loaded.--------------")
 	for line in f1:
 		if('    ' in line):
@@ -79,6 +93,38 @@ with open('data_100.ttl','r', buffering = 4096) as f1:
 				else:
 					print("Well, this is embarrassing... mold="+str(mold))
 #######################################################################################################			
+			elif(mold == 3):
+				if('rdf:type' in line):
+					pass
+				elif('rdfs:label' in line):
+					label = line.replace("    rdfs:label ","")
+					label = label.replace(" ;","")
+				elif('rdfs:comment' in line):
+					comment = line.replace("    rdfs:comment ","")
+					comment = comment.replace(" ;","")
+				elif('foaf:homepage' in line):
+					homepage = line.replace("    foaf:homepage <","")
+					homepage = homepage.replace("> ;","")
+				elif('bsbm:country' in line):
+					country = line.replace("    bsbm:country <","")
+					country = country.replace("> ;","")
+				elif('dc:publisher' in line):
+					pass
+				elif('dc:date' in line):
+					date = line.replace("    dc:date ","")
+					date = date.replace("^^xsd:date .","")
+					# QUERY
+					session.run("CREATE ("+header+":Producer {header:\""+header+"\",label:"+label+",comment:"+comment+",date:"+date+"})")
+					session.run("CREATE (homepage:Homepage {header:\""+homepage+"\"})")
+					if(checkCountry(country) == False):
+						session.run("CREATE (country:Country {header:\""+country+"\"})")
+					session.run("MATCH (son{header:\""+header+"\"}),(father{header:\""+homepage+"\"}) CREATE (son)-[:homepage]->(father)")
+					session.run("MATCH (son{header:\""+header+"\"}),(father{header:\""+country+"\"}) CREATE (son)-[:country]->(father)")
+				else:
+					print("Well, this is embarrassing... mold="+str(mold))
+#######################################################################################################			
+			# elif(mold == 4):
+#######################################################################################################			
 			else:
 				print ("--------------Closing program.--------------")
 				# session.close()
@@ -93,6 +139,16 @@ with open('data_100.ttl','r', buffering = 4096) as f1:
 			elif('bsbm-inst:ProductFeature' in line):
 				header = line.replace("bsbm-inst:","")
 				mold = 2
+			elif('dataFromProducer' in line):
+				if('Producer' in line):
+					header = line.split(':')[1]
+					mold = 3
+				else:
+					pass
+				# Work on mold 4
+				# else:
+				# 	header = line.split(':')[1]
+				# 	mold = 4
 			else:
 				mold = 0
 
